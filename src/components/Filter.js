b9d647e2b0ab5bfc3dc563fe4801daa17612b807
juan <juansoussev@gmail.com>
2023-07-30 15:47:08 -0300

    Tela de Filtro

---
+++ src/components/Filter.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { TouchableOpacity, Text, StyleSheet, Dimensions } from 'react-native'
+import Ionicons from '@expo/vector-icons/Ionicons'
+
+export default class FilterComponent extends Component {
+  render() {
+    return (
+      <TouchableOpacity
+        style={{
+          width: Dimensions.get('window').width / 3,
+          height: 20,
+          backgroundColor: this.props.color || '#d9d9d9',
+          borderRadius: 10,
+          alignItems: 'center',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          margin: 5
+        }}
+        onPress={this.props.add}>
+        <Text style={{ color: '#343a40' }}>
+          {this.props.value}  {this.props.search ? <Ionicons name='search' size={15} color='#343a40' /> : false}
+        </Text>
+
+      </TouchableOpacity>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
--- src/views/FilterScreen.js
+++ src/views/FilterScreen.js
@@ -1,10 +1,62 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet, FlatList } from 'react-native'
+import FilterComponent from '../components/Filter'
 
 export default class Filter extends Component {
+  add = (nome) => {
+    console.warn('adicionou')
+  }
   render() {
     return (
-      <Text>Filter Screen</Text>
+      <View style={styles.container}>
+        <View style={styles.filterArea}>
+          <View style={{ height: 70 }}>
+
+          </View>
+          <View style={{ width: '100%' , alignItems: 'flex-end' }}>
+            <FilterComponent value='Pesquisar' color='#0dcaf0' search />
+          </View>
+        </View>
+        <Text style={styles.filterBy}>Filter By</Text>
+        <View style={styles.filterContainer}>
+          <FilterComponent value='asd' add={this.add} />
+          <FilterComponent value='asd' add={this.add} />
+          <FilterComponent value='asd' add={this.add} />
+          <FilterComponent value='asd' add={this.add} />
+          <FilterComponent value='asd' add={this.add} />
+          <FilterComponent value='asd' add={this.add} />
+          <FilterComponent value='asd' add={this.add} />
+          <FilterComponent value='asd' add={this.add} />
+          <FilterComponent value='asd' add={this.add} />
+          <FilterComponent value='asd' add={this.add} />
+          <FilterComponent value='asd' add={this.add} />
+        </View>
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  filterContainer: {
+    width: '80%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  },
+  filterBy: {
+    width: '80%',
+    marginBottom: 10,
+    fontSize: 20
+  },
+  filterArea: {
+    width: '80%',
+    height: 100,
+    borderBottomWidth: 1,
+    borderBottomColor: '#343a40',
+    marginBottom: 10
+  }
+})
